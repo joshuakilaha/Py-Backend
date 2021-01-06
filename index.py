7aa0e6cb002dbd49987e1489dabc74c14c11fbea
joshuakilaha <jokilaha191@gmail.com>
2021-01-06 19:40:22 +0300

    Query Parameters

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import tornado.web
 import tornado.ioloop
 
+
 class basicRequestHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world this is python GET Request")
@@ -11,10 +12,22 @@ class carsRequestHander(tornado.web.RequestHandler):
         self.render("index.html")
 
 
+class queryRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        num = self.get_argument("num")
+        if num.isdigit():
+            r = "odd" if int(num) % 2 else "even"
+            self.write(f"The integer {num} is {r}")
+
+        else:
+            self.write(f"{num} is not an integer")
+
+
 if __name__ == "__main__":
     app = tornado.web.Application([
         (r"/", basicRequestHandler),
-        (r"/cars",carsRequestHander)
+        (r"/cars", carsRequestHander),
+        (r"/isEven", queryRequestHandler)
     ])
 
     port = 3000
